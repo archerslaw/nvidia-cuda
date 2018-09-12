8241101009974c67632b8b0c2d5bd1fe7a9ddee9
Archers Law <archerslaw@163.com>
2018-09-12 17:31:10 +0800

    Update cloudinit-centos-cuda-driver.sh

--- cloudinit-centos-cuda-driver.sh
+++ cloudinit-centos-cuda-driver.sh
@@ -189,7 +189,7 @@ create_nvidia_repo_centos >> $log 2>&1
 
 #curl -o /etc/yum.repos.d/hwCentOS-Base.repo http://mirrors.myhuaweicloud.com/repo/CentOS-Base-7.repo
 #curl -o /etc/yum.repos.d/nvidia-RHEL.repo http://119.3.60.246/ecs/linux/nvidia-RHEL.repo
-#yum install epel -y >> $log 2>&1
+yum install epel-release -y >> $log 2>&1
 
 #cp /etc/yum.repos.d/epel.repo /etc/yum.repos.d/hwepel.repo
 #cp /etc/yum.repos.d/epel-testing.repo /etc/yum.repos.d/hwepel-testing.repo
