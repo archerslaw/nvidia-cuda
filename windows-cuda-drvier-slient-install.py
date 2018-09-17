b43405e45fb12ba649ebc4c070105ea3a0ce194c
Archers Law <archerslaw@163.com>
2018-09-17 19:08:35 +0800

    Update windows-cuda-drvier-slient-install.py

--- windows-cuda-drvier-slient-install.py
+++ windows-cuda-drvier-slient-install.py
@@ -4,7 +4,7 @@ set PATH="C:\Program Files\Cloudbase Solutions\Cloudbase-Init\Python"
 echo import requests >C:\download.py
 echo file_url = "http://192.168.0.91/ecs/windows/exe/win2008r2/cuda/cuda_8.0.61_windows.exe" >>C:\download.py
 echo r = requests.get(file_url, stream=True) >>C:\download.py
-echo with open("C:\cuda_8.0.61_windows.exe", "wb") as pdf: >>C:\download.py
+echo with open("C:\cuda_8.0.61_windows.exe", "wb") as downloadfile: >>C:\download.py
 echo     for chunk in r.iter_content(chunk_size=1024): >>C:\download.py
 echo         if chunk: >>C:\download.py
 echo             pdf.write(chunk) >>C:\download.py
