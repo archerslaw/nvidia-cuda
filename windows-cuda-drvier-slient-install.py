6aaa5c1f8d0f4677a030ae7389c128b854b6e4d2
Archers Law <archerslaw@163.com>
2018-09-17 19:13:03 +0800

    Update windows-cuda-drvier-slient-install.py

--- windows-cuda-drvier-slient-install.py
+++ windows-cuda-drvier-slient-install.py
@@ -7,6 +7,6 @@ echo r = requests.get(file_url, stream=True) >>C:\download.py
 echo with open("C:\cuda_8.0.61_windows.exe", "wb") as downloadfile: >>C:\download.py
 echo     for chunk in r.iter_content(chunk_size=1024): >>C:\download.py
 echo         if chunk: >>C:\download.py
-echo             pdf.write(chunk) >>C:\download.py
+echo             downloadfile.write(chunk) >>C:\download.py
 python C:\download.py
 start /wait C:\cuda_8.0.61_windows.exe /s /v
