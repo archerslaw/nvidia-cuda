e7c20cb93002f78fbe35a9716e43d22cd1291140
Archers Law <archerslaw@163.com>
2018-09-17 17:42:00 +0800

    Update windows-cuda-drvier-slient-install.py

--- windows-cuda-drvier-slient-install.py
+++ windows-cuda-drvier-slient-install.py
@@ -1,12 +1,15 @@
 rem cmd
-set PATH=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\Python
+set PATH="C:\Program Files\Cloudbase Solutions\Cloudbase-Init\Python"
 
-echo import urllib2 >C:\download.py
-echo print "downloading with urllib2" >>C:\download.py
-echo url = 'http://119.3.60.246/ecs/windows/exe/win2008r2/cuda/cuda_8.0.61_windows.exe' >>C:\download.py
-echo f = urllib2.urlopen(url) >>C:\download.py
-echo data = f.read() >>C:\download.py
-echo with open("C:\cuda_8.0.61_windows.exe", "wb") as code: >>C:\download.py
-echo     code.write(data) >>C:\download.py
+echo import requests >C:\download.py
 
-start python C:\download.py
+echo file_url = "http://119.3.60.246/ecs/windows/exe/win2008r2/cuda/cuda_8.0.61_windows.exe" >>C:\download.py
+
+echo r = requests.get(file_url, stream=True) >>C:\download.py
+
+echo with open("python.pdf", "wb") as pdf: >>C:\download.py
+echo     for chunk in r.iter_content(chunk_size=1024): >>C:\download.py
+echo         if chunk: >>C:\download.py
+echo             pdf.write(chunk) >>C:\download.py
+
+python C:\download.py
