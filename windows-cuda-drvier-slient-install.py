310cb584d9ded98d6882e6cb8db32b00c05e8114
Archers Law <archerslaw@163.com>
2018-09-25 12:08:36 +0800

    Update windows-cuda-drvier-slient-install.py

--- windows-cuda-drvier-slient-install.py
+++ windows-cuda-drvier-slient-install.py
@@ -2,7 +2,7 @@ rem cmd
 
 set PATH="C:\Program Files\Cloudbase Solutions\Cloudbase-Init\Python"
 echo import requests >C:\nvidia-download.py
-echo file_url = "http://192.168.0.91/ecs/windows/exe/win2008r2/cuda/cuda_8.0.61_windows.exe" >>C:\nvidia-download.py
+echo file_url = "http://mirrors.myhuaweicloud.com/ecs/windows/exe/win2008r2/cuda/cuda_8.0.61_windows.exe" >>C:\nvidia-download.py
 echo r = requests.get(file_url, stream=True) >>C:\nvidia-download.py
 echo with open("C:\cuda_8.0.61_windows.exe", "wb") as downloadfile: >>C:\nvidia-download.py
 echo     for chunk in r.iter_content(chunk_size=1024): >>C:\nvidia-download.py
