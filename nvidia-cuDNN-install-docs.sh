43b064f8742627b4d8e8e8bf4218cede11305863
Archers Law <archerslaw@163.com>
2018-09-26 16:38:00 +0800

    Update nvidia-cuDNN-install-docs.sh

--- nvidia-cuDNN-install-docs.sh
+++ nvidia-cuDNN-install-docs.sh
@@ -12,8 +12,11 @@ chmod a+r /usr/local/cuda/include/cudnn.h  /usr/local/cuda/lib64/libcudnn*
 rm -fr cudnn-9.0-linux-x64-v7*
 
 # Update your environment variables in bash session or put them in your .bashrc file
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64/:$LD_LIBRARY_PATH
-export LIBRARY_PATH=/usr/local/cuda/lib64/:$LIBRARY_PATH
+# vim ~/.bashrc
+export CUDA_HOME=/usr/local/cuda
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib:$LD_LIBRARY_PATH
 
 # install pip for centos6.x+7.x|rhel6.x+7.x
 yum install python*pip -y
@@ -28,3 +31,6 @@ pip install --upgrade pip
 
 # install tensorflow-gpu
 pip install tensorflow-gpu==1.6.0 --trusted-host mirrors.myhuaweicloud.com -i http://mirrors.myhuaweicloud.com/pypi/web/simple
+
+
+
