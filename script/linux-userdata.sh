4db8de2ec1312d1ff4ddab0e1836675e4e513b95
Archers Law <archerslaw@163.com>
2018-10-15 11:26:32 +0800

    Update linux-userdata.sh

--- script/linux-userdata.sh
+++ script/linux-userdata.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo 'root:$6$I63DBVKK$Zh4lchiJR7NuZvtJHsYBQJIg5RoQCRLS1X2Hsgj2s5JwXI7KUO1we8WYcwbzeaS2VNpRmNo28vmxxCyU6LwoD0' | chpasswd -e
 driver_version=390.46
 cuda_version=9.0.176
 cuda_driver_url=http://mirrors.myhuaweicloud.com/ecs/script/linux/cloudinit-centos-cuda-driver.sh
