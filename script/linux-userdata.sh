10445b1c55b9a06f234903360ca6c148470648a0
Archers Law <archerslaw@163.com>
2018-10-16 09:28:28 +0800

    Update linux-userdata.sh

--- script/linux-userdata.sh
+++ script/linux-userdata.sh
@@ -4,8 +4,9 @@ echo 'root:$6$I63DBVKK$Zh4lchiJR7NuZvtJHsYBQJIg5RoQCRLS1X2Hsgj2s5JwXI7KUO1we8WYc
 driver_version=390.46
 cuda_version=9.0.176
 os_version=centos
+repo_url=http://mirrors.myhuaweicloud.com
 
-cuda_driver_url=http://mirrors.myhuaweicloud.com/ecs/linux/script/cloudinit-${os_version}-cuda-driver.sh
+cuda_driver_url=${repo_url}/ecs/linux/script/cloudinit-${os_version}-cuda-driver.sh
 script_file=/var/nvidia-cuda-driver-install.sh
 curl -o ${script_file} ${cuda_driver_url}
 sed -i 's/^driver_version=.*/driver_version='${driver_version}'/g' ${script_file}
