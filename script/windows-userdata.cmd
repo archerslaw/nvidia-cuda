627ad3ab38e24c20c1dad35748ae381d03aa0848
Archers Law <archerslaw@163.com>
2018-11-22 12:00:03 +0800

    Update windows-userdata.cmd

--- script/windows-userdata.cmd
+++ script/windows-userdata.cmd
@@ -7,6 +7,7 @@ set repo_url=http://mirrors.myhuaweicloud.com
 
 echo cuda_url="%repo_url%/ecs/windows/exe/%os_version%/cuda/cuda_%cuda_version%_windows.exe">C:\nvidia-download.py
 echo driver_url="%repo_url%/ecs/windows/exe/%os_version%/driver/%driver_version%-tesla-desktop-winserver-international.exe">>C:\nvidia-download.py
+echo script_url="http://mirrors.myhuaweicloud.com/ecs/windows/script/nvidia-auto-install.cmd">>C:\nvidia-download.py
 rem ===================================
 echo import requests>>C:\nvidia-download.py
 echo def nvidia_download(file_url, localfile_name):>>C:\nvidia-download.py
@@ -17,6 +18,7 @@ echo             if chunk:>>C:\nvidia-download.py
 echo                 downloadfile.write(chunk)>>C:\nvidia-download.py
 echo                 downloadfile.flush()>>C:\nvidia-download.py
 rem ===================================
+echo nvidia_download(script_url, 'C:\\Program Files\\Cloudbase Solutions\\Cloudbase-Init\\LocalScripts\\nvidia-auto-install.cmd')>>C:\nvidia-download.py
 echo nvidia_download(cuda_url, 'C:\\nvidia_cuda.exe')>>C:\nvidia-download.py
 echo nvidia_download(driver_url, 'C:\\nvidia_driver.exe')>>C:\nvidia-download.py
 rem ===================================
@@ -24,26 +26,4 @@ echo %DATE% %TIME% Logs: Start to download nvidia cuda and driver package>C:\nvi
 "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\Python\python.exe" C:\nvidia-download.py>>C:\nvidia_install_log.txt
 echo %DATE% %TIME% Logs: Download nvidia cuda and driver package success>>C:\nvidia_install_log.txt
 rem ===================================
-echo rem cmd>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-echo echo %%DATE%% %%TIME%% Logs: Install nvidia driver package^>^>C:\nvidia_install_log.txt>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-echo start /high /wait C:\nvidia_driver.exe -s^>^>C:\nvidia_install_log.txt>>C:\nvidia_install_log.txt>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-echo ping 127.0.0.1 -n 1 ^>nul>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-rem ===================================
-echo echo %%DATE%% %%TIME%% Logs: Install nvidia cuda package^>^>C:\nvidia_install_log.txt>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-echo start /high /wait C:\nvidia_cuda.exe -s^>^>C:\nvidia_install_log.txt>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-rem ===================================
-echo echo %%DATE%% %%TIME%% Logs: Install nvidia driver and cuda package success^>^>C:\nvidia_install_log.txt>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-rem ===================================
-echo del /a/f C:\nvidia-download.py^>^>C:\nvidia_install_log.txt>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-echo echo %%DATE%% %%TIME%% Logs: Remove nvidia-download.py script^>^>C:\nvidia_install_log.txt>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-rem ===================================
-echo del /a/f C:\nvidia_cuda.exe^>^>C:\nvidia_install_log.txt>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-echo echo %%DATE%% %%TIME%% Logs: Remove nvidia cuda package^>^>C:\nvidia_install_log.txt>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-rem ===================================
-echo del /a/f C:\nvidia_driver.exe^>^>C:\nvidia_install_log.txt>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-echo echo %%DATE%% %%TIME%% Logs: Remove nvidia driver package^>^>C:\nvidia_install_log.txt>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-rem ===================================
-echo echo %%DATE%% %%TIME%% Logs: Remove nvidia cuda and driver package success^>^>C:\nvidia_install_log.txt>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
-rem ===================================
-echo exit>>"C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts"\nvidia-install.cmd
 exit 0
