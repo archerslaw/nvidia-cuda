3ca9275029ad4a265ad319fc844d80798af0df13
Archers Law <archerslaw@163.com>
2018-11-22 14:52:06 +0800

    Update nvidia-auto-install.cmd

--- script/linux/nvidia-auto-install.cmd
+++ script/linux/nvidia-auto-install.cmd
@@ -1,19 +1,20 @@
 rem cmd
-echo %DATE% %TIME% Logs: Begin to auto install nvidia driver and cuda package>>C:\nvidia_install_log.txt
+echo %DATE% %TIME% Logs: Begin to auto install nvidia cuda and driver package>>C:\nvidia_install_log.txt
 start /high /wait C:\nvidia_driver.exe -s>>C:\nvidia_install_log.txt
-if %errorlevel% == 0 (
-    echo %DATE% %TIME% Logs: Auto install nvidia driver package>>C:\nvidia_install_log.txt
+if %errorlevel%==0 (
+    echo %DATE% %TIME% Logs: End to auto install nvidia driver package>>C:\nvidia_install_log.txt
     del /a/f C:\nvidia_driver.exe>>C:\nvidia_install_log.txt
-    echo %DATE% %TIME% Logs: Remove nvidia cuda package>>C:\nvidia_install_log.txt
+    echo %DATE% %TIME% Logs: Remove nvidia driver package>>C:\nvidia_install_log.txt
 ) else (
-    echo %DATE% %TIME% Logs: Fail to auto install nvidia driver package, you could install the C:\nvidia_driver.exe manually>>C:\nvidia_install_log.txt
+    echo %DATE% %TIME% Logs: Fail to auto install nvidia driver package, you could install the C:\nvidia_driver.exe and C:\nvidia_cuda.exe manually>>C:\nvidia_install_log.txt
+    exit
 )
 ping 127.0.0.1 -n 1 >nul
 start /high /wait C:\nvidia_cuda.exe -s>>C:\nvidia_install_log.txt
-if %errorlevel% == 0 (
-    echo %DATE% %TIME% Logs: Auto install nvidia cuda package>>C:\nvidia_install_log.txt
+if %errorlevel%==0 (
+    echo %DATE% %TIME% Logs: End to auto install nvidia cuda package>>C:\nvidia_install_log.txt
     del /a/f C:\nvidia_cuda.exe>>C:\nvidia_install_log.txt
-    echo %DATE% %TIME% Logs: Remove nvidia driver package>>C:\nvidia_install_log.txt
+    echo %DATE% %TIME% Logs: Remove nvidia cuda package>>C:\nvidia_install_log.txt
 ) else (
     echo %DATE% %TIME% Logs: Fail to auto install nvidia cuda package, you could install the C:\nvidia_cuda.exe manually>>C:\nvidia_install_log.txt
 )
