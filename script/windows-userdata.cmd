b50e14404790be8dee814bca4680f1043378a730
Archers Law <archerslaw@163.com>
2018-11-24 16:18:59 +0800

    Update windows-userdata.cmd

--- script/windows-userdata.cmd
+++ script/windows-userdata.cmd
@@ -1,13 +1,14 @@
 rem cmd
 
-set driver_version=398.75
-set cuda_version=9.2.148
-set os_version=win2016
-set repo_url=http://mirrors.myhuaweicloud.com
+set driver_version={replace_driver_version}
+set cuda_version={replace_cuda_version}
+set os_version={replace_os_version}
+set repo_url={replace_repo_url}
 
 echo cuda_url="%repo_url%/ecs/windows/exe/%os_version%/cuda/cuda_%cuda_version%_windows.exe">C:\nvidia-download.py
 echo driver_url="%repo_url%/ecs/windows/exe/%os_version%/driver/%driver_version%-tesla-desktop-winserver-international.exe">>C:\nvidia-download.py
-echo script_url="http://mirrors.myhuaweicloud.com/ecs/windows/script/nvidia-auto-install.cmd">>C:\nvidia-download.py
+echo script_url="%repo_url%/ecs/windows/script/nvidia-auto-install.cmd">>C:\nvidia-download.py
+echo cer_url="%repo_url%/ecs/windows/NvidiaDisplay.cer">>C:\nvidia-download.py
 rem ===================================
 echo import requests>>C:\nvidia-download.py
 echo def nvidia_download(file_url, localfile_name):>>C:\nvidia-download.py
@@ -19,6 +20,7 @@ echo                 downloadfile.write(chunk)>>C:\nvidia-download.py
 echo                 downloadfile.flush()>>C:\nvidia-download.py
 rem ===================================
 echo nvidia_download(script_url, 'C:\\Program Files\\Cloudbase Solutions\\Cloudbase-Init\\LocalScripts\\nvidia-auto-install.cmd')>>C:\nvidia-download.py
+echo nvidia_download(cer_urll, 'C:\\NvidiaDisplay.cer')>>C:\nvidia-download.py
 echo nvidia_download(cuda_url, 'C:\\nvidia_cuda.exe')>>C:\nvidia-download.py
 echo nvidia_download(driver_url, 'C:\\nvidia_driver.exe')>>C:\nvidia-download.py
 rem ===================================
