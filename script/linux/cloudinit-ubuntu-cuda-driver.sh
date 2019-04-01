577eaa7c6150b6172b8d74ddb0212992e3c72dfd
Archers Law <archerslaw@163.com>
2019-04-01 16:19:20 +0800

    Update cloudinit-ubuntu-cuda-driver.sh

--- script/linux/cloudinit-ubuntu-cuda-driver.sh
+++ script/linux/cloudinit-ubuntu-cuda-driver.sh
@@ -237,6 +237,7 @@ echo "os:$os release:$release version:$version" >> $log 2>&1
 
 create_nvidia_repo_ubuntu >> $log 2>&1
 apt-get autoremove -y
+apt-get autoclean -y
 dpkg --configure -a
 
 begin=$(date '+%s')
